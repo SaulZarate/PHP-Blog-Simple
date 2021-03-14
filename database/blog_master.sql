c29e3af662636b1ca2b5cd180271556c10dfde8b
saul zarate <saul.zarate@davinci.edu.ar>
2021-03-14 12:42:25 -0300

    Base de datos agregada

---
+++ database/blog_master.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3308
+-- Tiempo de generación: 14-03-2021 a las 15:41:06
+-- Versión del servidor: 8.0.18
+-- Versión de PHP: 7.4.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `blog_master`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`) VALUES
+(1, 'Video Juegos'),
+(2, 'Musica');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+DROP TABLE IF EXISTS `entradas`;
+CREATE TABLE IF NOT EXISTS `entradas` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `usuario_id` int(255) NOT NULL,
+  `categoria_id` int(255) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` mediumtext,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_entrada_usuario` (`usuario_id`),
+  KEY `fk_entrada_categorias` (`categoria_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `entradas`
+--
+
+INSERT INTO `entradas` (`id`, `usuario_id`, `categoria_id`, `titulo`, `descripcion`, `fecha`) VALUES
+(1, 1, 1, 'GTA 5', 'Es el juego mas vendido de todas las versiones', '2020-07-26'),
+(2, 1, 1, 'FIFA 20', 'Este juego es de futbol ', '2020-07-26'),
+(3, 2, 1, 'Call Of Duty', 'Este juego es de accion , disparios. Se podria decir que una version mejorada de Counter Striker', '2020-07-26'),
+(4, 2, 2, 'Sinfonia N°6 de Tchaicovsky', 'Esta sinfonia tambien es conocida con el nombre de la Patetica de Tchaicovsky', '2020-07-26'),
+(5, 2, 2, 'Sinfonia N°5 Mahler', 'Esta sinfonia tiene una duracion aproximada de 1 hora y 10 minutos.', '2020-07-26');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `email`, `password`, `fecha`) VALUES
+(1, 'saul', 'zarate', 'saul@gmail.com', '$2y$04$d8ygoR9dD8.F9tZoy0lr4Odbazs202.492wwHR1W1z4TNvDFsE7FS', '2020-07-26'),
+(2, 'Pepe', 'Argento', 'pepito@gmail.com', '$2y$04$56DJdHYelYsso50esg2CdepW5YGltt6prC9HTGeba.fo57L4hmBEe', '2020-07-26');
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `entradas`
+--
+ALTER TABLE `entradas`
+  ADD CONSTRAINT `fk_entrada_categorias` FOREIGN KEY (`categoria_id`) REFERENCES `categorias` (`id`),
+  ADD CONSTRAINT `fk_entrada_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
