70a016d6aa7d2447fcc8301914d739f74c74a570
saul zarate <saul.zarate@davinci.edu.ar>
2021-03-14 12:36:54 -0300

    Proyecto de Blog terminado

---
+++ includes/pie.php
@@ -0,0 +1,11 @@
+
+			<div class="clearfix"></div>
+		</div> <!-- fin contenedor -->
+
+		<!-- PIE DE PÁGINA -->
+		<footer id="pie">
+			<p>Desarrollado por Saul Zarate &copy; <?=date('Y',time())?></p>
+		</footer>
+		
+	</body>
+</html>
